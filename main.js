cccba30d45bc467bf78a9af50abdb9b2bcc1092d
adhyuva <adhyuva8@gmail.com>
2021-06-20 11:04:50 -0400

    Making the fishy change directions

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 
 let x = 60 ;
 let xPx = x + "px";
-let y = 60 ;
-let yPx = y + "px";
+$("#fish2").hide();
 function eventHandler(event) {
     if(event.keyCode == 39){
         x += 10;
         xPx = x + "px";
         $("#box").css("left", xPx)
-
+        document.getElementById("fish2").visibility = "hidden";
+        $("#fish2").hide();
+        $("#fish").show();
     }else if(event.keyCode == 37){
         x -= 10;
         xPx = x + "px";
         $("#box").css("left", xPx)
-    }else if(event.keyCode == 32 || event.keyCode == 38){
-        y -= 10;
-        yPx = y + "px";
-        $("#box").css("top", yPx)
-    }else if(event.keyCode == 40){
-        y += 10;
-        yPx = y + "px";
-        $("#box").css("top", yPx)
+        $("#fish").hide();
+        $("#fish2").show();
     }else{
     }
 }
